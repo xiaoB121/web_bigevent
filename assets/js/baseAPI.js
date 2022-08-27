da64ce1cd005625454e967e7aa2f327c9e825a8b
xiaoB <1622656960@qq.com>
2022-08-27 21:27:12 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,4 +5,23 @@ $.ajaxPrefilter(function (options) {
     // console.log(options.url);
     //发起真正的ajax请求之前，同一凭借根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url;
+
+    // 统一为有权限的接口，设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || '',
+        }
+    }
+
+    // 统一挂载 complete 回调函数
+    options.complete = function (res) {
+        // 在complete回调函数，可以使用res.responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1.强制清空token
+            localStorage.removeItem('token');
+            // 2.强制回到登录页面
+            location.href = '/login.html';
+        }
+        console.log(res);
+    }
 });
